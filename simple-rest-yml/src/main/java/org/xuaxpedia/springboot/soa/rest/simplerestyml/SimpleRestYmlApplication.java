9679546c33f2960885854797d95b8530279659a0
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-26 18:15:25 -0500

    Remove the explicit access restriction

--- simple-rest-yml/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestyml/SimpleRestYmlApplication.java
+++ simple-rest-yml/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestyml/SimpleRestYmlApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleRestYmlApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(SimpleRestYmlApplication.class, args);
 	}
 
