482f4596fc84e642ae5df4cb43338660cde89371
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-27 10:51:08 -0500

    refactor(jaxb): remove legacy package and generated classes

--- simple-soap-yml/src/main/java/org/xuaxpedia/springboot/soa/soap/simplesoapyml/xml/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.xuaxpedia.org/springboot/soa/soap/simplesoapyml/xml", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package org.xuaxpedia.springboot.soa.soap.simplesoapyml.xml;
