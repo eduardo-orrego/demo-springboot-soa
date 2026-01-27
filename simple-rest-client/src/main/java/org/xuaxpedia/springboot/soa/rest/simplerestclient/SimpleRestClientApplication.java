7be5d08e1d1196caa9b0593a8026980e4d50759e
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-27 15:33:26 -0500

    Remove the explicit access restriction

--- simple-rest-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestclient/SimpleRestClientApplication.java
+++ simple-rest-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestclient/SimpleRestClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleRestClientApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(SimpleRestClientApplication.class, args);
 	}
 
