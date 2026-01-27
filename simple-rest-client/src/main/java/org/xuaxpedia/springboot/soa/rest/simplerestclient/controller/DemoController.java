2f8ee8442f0bfd000ead2ab7349876cdf458878b
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-27 18:25:57 -0500

    The @ResponseStatus annotation is added and the ResponseEntity class is removed.

--- simple-rest-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestclient/controller/DemoController.java
+++ simple-rest-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplerestclient/controller/DemoController.java
@@ -2,7 +2,10 @@ package org.xuaxpedia.springboot.soa.rest.simplerestclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.xuaxpedia.springboot.soa.rest.simplerestclient.model.DemoModel;
 import org.xuaxpedia.springboot.soa.rest.simplerestclient.service.DemoService;
 
@@ -19,10 +22,16 @@ public class DemoController {
     return ResponseEntity.ok().body(demoModel);
   }
 
+  /*
+   * ResponseEntity is not used because the HTTP status is fixed (201)
+   * and no custom headers are required.
+   * Spring automatically serializes the object to JSON and applies
+   * the status defined by @ResponseStatus.
+   * This code is for educational/demo purposes only.
+   */
   @GetMapping("/client/rest/wc/{id}")
-  public ResponseEntity<DemoModel> getRestWC(@PathVariable Long id) {
-    DemoModel demoModel = demoService.getDataRestWC(id);
-    return ResponseEntity.ok().body(demoModel);
+  public DemoModel getRestWC(@PathVariable Long id) {
+    return demoService.getDataRestWC(id);
   }
 
 }
