6729199ec25214c2fe0ddcc2f4ac72aea87cdc8d
Eduardo Orrego <eduardo.orrego@outlook.com>
2026-01-28 17:04:16 -0500

    Refactor SOAP client and configuration to use WebServiceTemplate with composition

--- simple-soap-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplesoapclient/client/SoapClient.java
+++ simple-soap-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplesoapclient/client/SoapClient.java
@@ -1,8 +1,7 @@
 package org.xuaxpedia.springboot.soa.rest.simplesoapclient.client;
 
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.stereotype.Component;
-import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.soap.client.core.SoapActionCallback;
 import org.xuaxpedia.springboot.soa.rest.simplesoapclient.generated.CreateDataXmlRequest;
 import org.xuaxpedia.springboot.soa.rest.simplesoapclient.generated.CreateDataXmlResponse;
@@ -10,22 +9,24 @@ import org.xuaxpedia.springboot.soa.rest.simplesoapclient.generated.GetDataXmlRe
 import org.xuaxpedia.springboot.soa.rest.simplesoapclient.generated.GetDataXmlResponse;
 
 @Component
-public class SoapClient extends WebServiceGatewaySupport {
+public class SoapClient {
 
-  public SoapClient(Jaxb2Marshaller marshaller) {
-    getWebServiceTemplate().setMarshaller(marshaller);
-    getWebServiceTemplate().setUnmarshaller(marshaller);
+  private final WebServiceTemplate wsTemplate;
+
+  public SoapClient(WebServiceTemplate webServiceTemplate) {
+    this.wsTemplate = webServiceTemplate;
   }
 
   public GetDataXmlResponse getDataSoapClient(GetDataXmlRequest request) {
-    return (GetDataXmlResponse) getWebServiceTemplate()
-      .marshalSendAndReceive("http://localhost:8084/service/demo-data", request,
-        new SoapActionCallback("http://www.xuaxpedia.org/springboot/soa/soap/simplesoapyml/xml/GetDataXmlRequest"));
+    String uriActionGet = "http://www.xuaxpedia.org/springboot/soa/soap/simplesoapyml/xml/GetDataXmlRequest";
+    Object objectResponse = wsTemplate.marshalSendAndReceive(request, new SoapActionCallback(uriActionGet));
+    return (GetDataXmlResponse) objectResponse;
   }
 
   public CreateDataXmlResponse createDataSoapClient(CreateDataXmlRequest request) {
-    return (CreateDataXmlResponse) getWebServiceTemplate()
-      .marshalSendAndReceive("http://localhost:8084/service/demo-data", request,
-        new SoapActionCallback("http://www.xuaxpedia.org/springboot/soa/soap/simplesoapyml/xml/CreateDataXmlRequest"));
+    String uriActionCreate = "http://www.xuaxpedia.org/springboot/soa/soap/simplesoapyml/xml/GetDataXmlRequest";
+    Object objectResponse = wsTemplate.marshalSendAndReceive(request, new SoapActionCallback(uriActionCreate));
+    return (CreateDataXmlResponse) objectResponse;
   }
+
 }
--- simple-soap-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplesoapclient/config/SoapConfig.java
+++ simple-soap-client/src/main/java/org/xuaxpedia/springboot/soa/rest/simplesoapclient/config/SoapConfig.java
@@ -1,16 +1,30 @@
 package org.xuaxpedia.springboot.soa.rest.simplesoapclient.config;
 
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
 
 @Configuration
 public class SoapConfig {
+
     @Bean
     public Jaxb2Marshaller marshaller() {
         Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
         marshaller.setContextPath("org.xuaxpedia.springboot.soa.rest.simplesoapclient.generated");
+        marshaller.setMarshallerProperties(Map.of(jakarta.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8"));
         return marshaller;
     }
 
+    @Bean
+    public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
+        WebServiceTemplate template = new WebServiceTemplate();
+        template.setMarshaller(marshaller);
+        template.setUnmarshaller(marshaller);
+        template.setDefaultUri("http://localhost:8084/service/demo-data");
+        return template;
+    }
+
 }
+
